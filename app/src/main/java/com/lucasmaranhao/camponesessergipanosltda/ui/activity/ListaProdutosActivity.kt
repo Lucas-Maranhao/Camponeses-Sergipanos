ab0d0b81dc8c4556232f9f8595dbd8c5268c1dca
Lucas Maranhão <Lucasvsm@gmail.com>
2022-06-24 16:54:30 -0300

    Implementando a activity de formulário para adicionar um produto a lista e fazendo a refatoração do código

---
+++ app/src/main/java/com/lucasmaranhao/camponesessergipanosltda/ui/activity/ListaProdutosActivity.kt
@@ -0,0 +1,44 @@
+package com.lucasmaranhao.camponesessergipanosltda.ui.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.lucasmaranhao.camponesessergipanosltda.R
+import com.lucasmaranhao.camponesessergipanosltda.dao.ProdutosDao
+import com.lucasmaranhao.camponesessergipanosltda.ui.recyclerview.adapter.ListaProdutosAdapter
+
+class ListaProdutosActivity : AppCompatActivity(R.layout.activity_lista_produtos) {
+
+    private val dao = ProdutosDao()
+    private val adapter = ListaProdutosAdapter(context = this, produtos = dao.buscaTodos())
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        configuraRecyclerView()
+        configuraFab()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adapter.atualiza(dao.buscaTodos())
+    }
+
+    private fun configuraFab() {
+        val fab = findViewById<FloatingActionButton>(R.id.activity_lista_produto_floatingActionButton)
+        fab.setOnClickListener {
+            vaiParaFormularioProduto()
+        }
+    }
+
+    private fun vaiParaFormularioProduto() {
+        val intent = Intent(this, FormularioProdutoActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun configuraRecyclerView() {
+        val recyclerview = findViewById<RecyclerView>(R.id.activity_lista_produto_recyclerview)
+        recyclerview.adapter = adapter
+    }
+}
